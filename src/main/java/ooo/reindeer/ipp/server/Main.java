7b3b68c6122e93b98dbd51b078f972e14b1744b1
Bailin <bailinsong@me.com>
2024-12-31 11:38:24 +0800

    feat(server): 增加打印队列属性支持
    - 添加了多个新的打印队列属性，如 colorSupported、copiesSupported 等
    - 实现了 allFieldsToKeyword 和 allFieldsToString 方法，用于动态生成属性值
    - 优化了代码结构，提高了可维护性和可扩展性

--- src/main/java/ooo/reindeer/ipp/server/Main.java
+++ src/main/java/ooo/reindeer/ipp/server/Main.java
@@ -1,19 +1,19 @@
 package ooo.reindeer.ipp.server;
 
-import com.hp.jipp.encoding.Attribute;
-import com.hp.jipp.encoding.IppPacket;
-import com.hp.jipp.encoding.StringType;
-import com.hp.jipp.encoding.Tag;
-import com.hp.jipp.model.JobState;
-import com.hp.jipp.model.JobStateReason;
+import com.hp.jipp.encoding.*;
+import com.hp.jipp.model.*;
 import kotlin.ranges.IntRange;
 import org.slf4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+import static com.hp.jipp.model.MediaType.stationery;
 import static com.hp.jipp.model.SystemConfiguredPrinters.printerInfo;
 import static com.hp.jipp.model.Types.*;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -23,7 +23,7 @@ public class Main {
     final static Logger logger = getLogger(Main.class);
 
     public static void main(String[] args) throws URISyntaxException {
-        IppPrinter printer = new IppPrinter("test03", "4fa29dc1-5209-4692-8a42-153d620e3c38", new IIppPrinterEventHandler() {
+        IppPrinter printer = new IppPrinter("test03", "urn:uuid:4fa29dc1-5209-4692-8a42-153d620e3c38", new IIppPrinterEventHandler() {
 
             final Logger logger = getLogger(IIppPrinterEventHandler.class);
 
@@ -86,17 +86,29 @@ public class Main {
 
         printer.putAttribute(new StringType(Tag.mimeMediaType, "document-format-preferred").of("application/pdf"));
 
-        printer.putAttribute(mediaTypeSupported.of("A4"));
-        printer.putAttribute(mediaSupported.of("A4"));
-        printer.putAttribute(mediaColSupported.of("A4"));
-        printer.putAttribute(documentFormatDetailsSupported.of("A4"));
+        printer.putAttribute(orientationRequestedSupported.of(Orientation.all.values()));
+
+        printer.putAttribute(colorSupported.of(true));
+        printer.putAttribute(copiesSupported.of(new IntRange(1,100)));
+        printer.putAttribute(coverBackSupported.of(CoverBack.media.getName(),CoverBack.mediaCol.getName(),CoverBack.coverType.getName()));
+        printer.putAttribute(coverFrontSupported.of(CoverBack.media.getName(),CoverBack.mediaCol.getName(),CoverBack.coverType.getName()));
+        printer.putAttribute(coverTypeSupported.of(allFieldsToString(CoverType.class)));
+        printer.putAttribute(destinationAccessesSupported.of(DestinationAccesses.accessUserName.getName(),DestinationAccesses.accessPassword.getName(),DestinationAccesses.accessOauthToken.getName(),DestinationAccesses.accessPin.getName()));
+        printer.putAttribute(finishingsSupported.of(Finishing.all.values()));
+        printer.putAttribute(printQualitySupported.of(PrintQuality.all.values()));
+        printer.putAttribute(printScalingSupported.of(allFieldsToString(PrintScaling.class)));
+        printer.putAttribute(sidesSupported.of(allFieldsToString(Sides.class)));
+        printer.putAttribute(mediaTypeSupported.of(allFieldsToKeyword(MediaType.class)));
+        printer.putAttribute(mediaSupported.of(allFieldsToKeyword(Media.class)));
+        printer.putAttribute(multipleDocumentHandlingSupported.of(allFieldsToString(MultipleDocumentHandling.class)));
         printer.putAttribute(jobPagesPerSetSupported.of(true));
         printer.putAttribute(jobImpressionsSupported.of(new IntRange(1, 100)));
         printer.putAttribute(jobPasswordEncryptionSupported.unsupported());
-        printer.putAttribute(multipleDocumentHandlingSupported.of("none"));
-        printer.putAttribute(orientationRequestedSupported.of(3));
-
-
+        printer.putAttribute(accuracyUnitsSupported.of(allFieldsToString(AccuracyUnit.class)));
+        printer.putAttribute(balingTypeSupported.of(allFieldsToKeyword(BalingType.class)));
+        printer.putAttribute(balingWhenSupported.of(allFieldsToString(BalingWhen.class)));
+        printer.putAttribute(bindingReferenceEdgeSupported.of(allFieldsToString(BindingReferenceEdge.class)));
+//        printer.putAttribute(bindingTypeSupported.of())
 
 
         try {
@@ -108,5 +120,31 @@ public class Main {
         } catch (IOException e) {
             logger.error("IppServer start error", e);
         }
+    }
+
+    public static List<KeywordOrName> allFieldsToKeyword(Class<?> clazz) {
+        return Arrays.stream(clazz.getFields())
+                .filter(field -> field.getType() == String.class && java.lang.reflect.Modifier.isStatic(field.getModifiers()))
+                .map(field -> {
+                    try {
+                        return new KeywordOrName((String) field.get(null));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> allFieldsToString(Class<?> clazz) {
+        return Arrays.stream(clazz.getFields())
+                .filter(field -> field.getType() == String.class && java.lang.reflect.Modifier.isStatic(field.getModifiers()))
+                .map(field -> {
+                    try {
+                        return (String) field.get(null);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
